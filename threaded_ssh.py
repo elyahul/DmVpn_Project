d11a59c9ec2a6d1dc7770b3b1dfe6858d2e4cafc
elyahul <lozovskyelyahu@gmail.com>
2020-05-11 13:01:18 -0700

    Added Logging

--- threaded_ssh.py
+++ threaded_ssh.py
@@ -9,40 +9,43 @@ from netmiko import NetMikoAuthenticationException
 import yaml
 from contracts import contract
 
+logger = logging.getLogger("MyLog")
 logging.getLogger('paramiko').setLevel(logging.WARNING)
-logging.basicConfig(
-    format = '%(threadName)s %(name)s: %(message)s',
-    level=logging.INFO)
-
-start_msg = '====> {} Connection: '
+formatter = logging.Formatter('## %(levelname)s LOG  %(threadName)s: %(message)s at %(asctime)s',
+                              datefmt='%H:%M:%S')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
 
 @contract(cfg = 'str[>0]', kwargs = dict)
 def send_show(kwargs, cfg):
     with ConnectHandler(**kwargs) as ssh:
-        logging.info(start_msg.format(datetime.now().time()))
+        logger.info('Is connecting to device {}'.format(device["ip"]))
         result = ssh.send_command(cfg)
         return result
 
 @contract(cfg = 'list[>0]', kwargs = dict)
 def send_config(kwargs, cfg):
     with ConnectHandler(**kwargs) as ssh:
-        logging.info(start_msg.format(datetime.now().time()))
-        result = ssh.send_config_set(*cfg)
-        return result
+        logger.info('Is connecting to device {}'.format(device["ip"]))
+        result = ssh.send_config_set(cfg)
+    return result
 
 def threads_conn(foo, device_list, limit, cfg_list):
     global output_list
     output_list = []
-    print(cfg_list)
     with ThreadPoolExecutor(max_workers=limit) as executor:
         futures = []
         for device,cfg in zip(device_list, cfg_list) :
             futures.append(executor.submit(foo, device, cfg))
+            logger.info('Device {} configuration is completed at'.format(device["ip"]))
         for future in as_completed(futures):
             try:
                 output_list.append(future.result())
             except NetMikoAuthenticationException as err:
                 print(future.exception())
-    return output_list
+        return output_list
 
     
